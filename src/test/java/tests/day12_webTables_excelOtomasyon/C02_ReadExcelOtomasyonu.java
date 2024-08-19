d883a3502ea0d820a371932599d49ad0b9fd9ce3
EsraAdileYilmaz <yilmazesraadile@gmail.com>
2024-08-19 20:53:08 +0200

    19/08

--- src/test/java/tests/day12_webTables_excelOtomasyon/C02_ReadExcelOtomasyonu.java
+++ src/test/java/tests/day12_webTables_excelOtomasyon/C02_ReadExcelOtomasyonu.java
@@ -12,7 +12,7 @@ public class C02_ReadExcelOtomasyonu {
 
 
     @Test
-   public void excelOtomasyonTesti() throws IOException {
+    public void excelOtomasyonTesti() throws IOException {
 
         /*
             Bilgisayarimizdaki fiziki bir dosyayi
@@ -26,7 +26,7 @@ public class C02_ReadExcelOtomasyonu {
             Biz de bu kopya excel uzerinde istedigimiz islemleri yapabiliriz.
 
             ONEMLI NOT :
-            bu test methodu icinde yaptigimiz tum islemler
+            Bu test methodu icinde yaptigimiz tum islemler
             excel uzerinde degil,
             olusturdugumuz Workbook objesinin bulundugu satirin (42.satirin)
             calistigi anda excel'de varolan bilgiler uzerindedir.
@@ -36,35 +36,21 @@ public class C02_ReadExcelOtomasyonu {
 
             Class icinde Workbook,Sheet,Row ve Cell objelerinin 1 kez olusturulmasi YETERLIDIR.
          */
-        String dosyaYolu="src/test/java/tests/day12_webTables_excelOtomasyon/ulkeler.xlsx";//indirilen excel dosyasinin dosya yolu.bu yol projedeki herkeste ayni olmalidir
+        String dosyaYolu = "src/test/java/tests/day12_webTables_excelOtomasyon/ulkeler.xlsx";//indirilen excel dosyasinin dosya yolu.bu yol projedeki herkeste ayni olmalidir
         //"user.dir" den sonraki kismi verdi.
-        FileInputStream fileInputStream=new FileInputStream(dosyaYolu);//dosya okuma objesi olusturuyoruz
-        Workbook workbook= WorkbookFactory.create(fileInputStream);//kopya excell'i olusturuyoruz
-        Sheet sayfa1=workbook.getSheet("Sayfa1");//excell'in icindeki islem yapmak istedigimiz sayfayi seciyoruz.
-        Row ucuncuSatir=sayfa1.getRow(3);// index=3, fiziki olarak 4.satira gider
-        Cell ikinciData=ucuncuSatir.getCell(2);// index=2, fiziki olarak 3.dataya gider
+        FileInputStream fileInputStream = new FileInputStream(dosyaYolu);//dosya okuma objesi olusturuyoruz
+        Workbook workbook = WorkbookFactory.create(fileInputStream);//kopya excell'i olusturuyoruz
+        Sheet sayfa1 = workbook.getSheet("Sayfa1");//excell'in icindeki islem yapmak istedigimiz sayfayi seciyoruz.
+        Row ucuncuSatir = sayfa1.getRow(3);// index=3, fiziki olarak 4.satira gider
+        Cell ikinciData = ucuncuSatir.getCell(2);// index=2, fiziki olarak 3.dataya gider
 
         System.out.println(ikinciData);//Cezayir
 
         // 12.satirda bulunan ulkenin turkce isminin "Azerbaycan" oldugunu test edin
-        String expectedUlkeAdi="Azerbaycan";
-        String actualIstenenUlkeAdi=sayfa1.getRow(11).getCell(2).toString();
+        String expectedUlkeAdi = "Azerbaycan";
+        String actualIstenenUlkeAdi = sayfa1.getRow(11).getCell(2).toString();
         //toString() methodu ile getirilen bilgiyi String'e cevirdik.cunku getirilen bilgi, Cell formundaydi.
-        Assert.assertEquals(expectedUlkeAdi,actualIstenenUlkeAdi);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        Assert.assertEquals(expectedUlkeAdi, actualIstenenUlkeAdi);
 
 
     }
--- src/test/java/tests/day12_webTables_excelOtomasyon/C04_ReadExcel.java
+++ src/test/java/tests/day12_webTables_excelOtomasyon/C04_ReadExcel.java
@@ -16,22 +16,21 @@ public class C04_ReadExcel {
     public void readExcelTesti() throws IOException {
 
         // ilgili ayarlamalari yapip, Sayfa2'yi acin
-        String dosyaYolu="src/test/java/tests/day12_webTables_excelOtomasyon/ulkeler.xlsx";
-        FileInputStream fileInputStream=new FileInputStream(dosyaYolu);//dosya okuma objesi
-        Workbook workbook= WorkbookFactory.create(fileInputStream);//kopya excel
-        Sheet sayfa2=workbook.getSheet("Sayfa2");//sayfa objesi olusturuldu ve getSheet("Sayfa2") ile calismak istedigimiz sayfayi belirledik
+        String dosyaYolu = "src/test/java/tests/day12_webTables_excelOtomasyon/ulkeler.xlsx";
+        FileInputStream fileInputStream = new FileInputStream(dosyaYolu);//dosya okuma objesi
+        Workbook workbook = WorkbookFactory.create(fileInputStream);//kopya excel
+        Sheet sayfa2 = workbook.getSheet("Sayfa2");//sayfa objesi olusturuldu ve getSheet("Sayfa2") ile calismak istedigimiz sayfayi belirledik
 
         // sayfada son kullanilan satirin 26.satir oldugunu test edin
-        int expectedSatirSayisi=26;
-        int actualSatirSayisi=sayfa2.getLastRowNum()+1;//indexi 0 dan basladigi icin +1 yaptik
+        int expectedSatirSayisi = 26;
+        int actualSatirSayisi = sayfa2.getLastRowNum() + 1;//indexi 0 dan basladigi icin +1 yaptik
         //getLastRowNum() methodu kullanilan en son satir numarasini getirir.
-        Assert.assertEquals(expectedSatirSayisi,actualSatirSayisi);
+        Assert.assertEquals(expectedSatirSayisi, actualSatirSayisi);
 
-        // sayfada reel olarak yazi yazilan(fiziksel olarak kullanilan) satir sayisinin 13 oldugunu test edin
-        int expectedFizikiKullanilanSatirSayisi=13;
-        int actualFizikiKullanilanSatirSayisi=sayfa2.getPhysicalNumberOfRows();
-        Assert.assertEquals(expectedFizikiKullanilanSatirSayisi,actualFizikiKullanilanSatirSayisi);
-
+        // sayfada reel olarak yazi yazilan(fiziksel olarak kullanilan) satir sayisinin 18 oldugunu test edin
+        int expectedFizikiKullanilanSatirSayisi = 18;
+        int actualFizikiKullanilanSatirSayisi = sayfa2.getPhysicalNumberOfRows();
+        Assert.assertEquals(expectedFizikiKullanilanSatirSayisi, actualFizikiKullanilanSatirSayisi);
 
 
     }
